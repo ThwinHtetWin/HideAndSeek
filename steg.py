ec36d0a52c7bfb0eb3787dba619f747281903405
ZuS <44851737+ThwinHtetWin@users.noreply.github.com>
2020-07-09 14:49:03 +0630

    Update steg.py

--- steg.py
+++ steg.py
@@ -1,25 +1,30 @@
 import cv2
 import numpy as np
 import os
+from tqdm import tqdm
 from cryptography.fernet import Fernet
-from colorama import init
-init()
 
-green = "\033[1;32m"
-end   = "\033[0;0m"
+BGreen="\033[1;32m"       # Green
+reset="\u001b[0m"         # Default 
+
 def main(): 
 
-    print(green+"                        [:: Steganography tool using LSB Method - "
-        "Presented by Group(2) ::]\n"+end)
-    a = int(input("\n=>  Enter ( 1 ) to Hide the data in the image\n"
-                    "=>  Enter ( 2 ) to Reveal the data from the encoded image : ")) 
-    if (a == 1): 
+    print("""
+    {c}H I D E & S E E K - Presented By Group-2
+    ----------------------------------------
+    """.format(c=BGreen))
+
+    print("""
+{g}[ HIDE ]{reset}  Enter ( 1 ) to Hide the data in the image
+{g}[ SEEK ]{reset}  Enter ( 2 ) to Reveal the data from the encoded image\n""".format(g=BGreen,reset=reset))
+    a = int(input("Hide/Seek : "))
+    if(a==1):  
         img_enc()
-        
-    elif (a == 2): 
+    elif(a==2):
         img_dec()
-    else: 
-        raise Exception("Enter correct input please!") 
+    else:
+        print("Please enter 1 or 2 .")
+        exit()
 
 
 def to_bin(data):
@@ -65,7 +70,7 @@ def encode(image_name, secret_data):
     return image
 
 def decode(image_name):
-    print("["+green+"!"+end+"] Please wait for 1 minute.\n["+green+"*"+end+"] Decoding image to retrieve the encrypted data...")
+    print("[{g}RESULT{reset}] Please wait for 1 minute.\n[{g}RESULT{reset}] Decoding image to retrieve the encrypted data...".format(g=BGreen,reset=reset))
     image = cv2.imread(image_name)
     binary_data = ""
     for row in image:
@@ -90,18 +95,17 @@ def img_enc():
     global clean_encoded_msg
     key=Fernet.generate_key()
     clean_key = key.decode('utf-8')
-    print("--------------------------------------------------------------")
-    print(green+"Access key : ",clean_key+end)
-    print("["+green+"!"+end+"] Keep this key in a safe place !\n--------------------------------------------------------------")
+    print("-"*60)
+    print("{g}Access key : ".format(g=BGreen),reset,clean_key)
+    print("[{g}Note{reset}] Keep this key in a safe place ! {g}[!]".format(reset=reset,g=BGreen))
+    print("-"*60,reset)
     fernet=Fernet(key)
 
-    secret_data = input("["+green+"+"+end+"] Enter your message(data) : ").encode("utf-8")
+    secret_data = input("[{g}TODO{reset}]   Enter your message(data) : ".format(g=BGreen,reset=reset)).encode("utf-8")
     encoded=fernet.encrypt(secret_data)
     clean_encoded_msg = encoded.decode('utf-8')
-    print("["+green+"*"+end+"]"+green+" Encrypted data           "+end+":",clean_encoded_msg,"\n")
-
-        
-    input_image = input("["+green+"*"+end+"] Enter your image         : ")
+    print("[{g}RESULT{reset}] Encrypted data           :".format(g=BGreen,reset=reset),clean_encoded_msg)
+    input_image = input("[{g}TODO{reset}]   Enter your image         : ".format(g=BGreen,reset=reset))
         # split the absolute path and the file
     path, file = os.path.split(input_image)
         # split the filename and the image extension
@@ -109,23 +113,22 @@ def img_enc():
     output_image = os.path.join(path, f"{filename}_encoded.{ext}")
     encoded_image = encode(image_name=input_image, secret_data=secret_data)       
     cv2.imwrite(output_image, encoded_image)
-    print("["+green+"*"+end+"]"+green+" Saved encoded image      : "+end+output_image,'\n')
+    print("[{g}RESULT{reset}] Saved encoded image      : ".format(g=BGreen,reset=reset),output_image,'\n')
 
 def img_dec():
-    print("--------------------------------------------------------------")
-    to_decode = input("["+green+"+"+end+"] Enter your encoded image : ")
+    print("-"*60)
+    to_decode = input("[{g}TODO{reset}]   Enter your encoded image : ".format(g=BGreen,reset=reset))
 
     decoded_data = decode(to_decode)
-    print("\n--------------------------------------------------------------")
-    print("["+green+"+"+end+"] Decoded data     :",decoded_data)
-    key=input("["+green+"+"+end+"] Enter access key : ").encode()
+    print("-"*60)
+    print("[{g}RESULT{reset}] Decoded data     :".format(g=BGreen,reset=reset),decoded_data)
+    key=input("[{g}TODO{reset}]   Enter access key : ".format(g=BGreen,reset=reset)).encode()
     f=Fernet(key)
 
     decoded_data=decoded_data.encode()
     decrypt = f.decrypt(decoded_data)
     clean_decrypt = decrypt.decode('utf-8')
-    print("\n["+green+"*"+end+"]"+green+" Decrypted data   "+end+":",clean_decrypt)
+    print("[{g}RESULT{reset}] Decrypted data   :".format(g=BGreen,reset=reset),clean_decrypt)
 
-if __name__ == '__main__' : 
-    
-    main()  
+#Here we goooo!!!
+main()  
