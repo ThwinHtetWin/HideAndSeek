568c61f5c353067e7442512db3bf83d0902e0585
ThwinHtetWin <44851737+ThwinHtetWin@users.noreply.github.com>
2020-11-26 14:28:39 +0630

    Update steg.py

--- steg.py
+++ steg.py
@@ -5,6 +5,7 @@ import numpy as np
 from PIL import Image
 from cryptography.fernet import Fernet
 
+Cyan="\033[0;36m"
 Green="\033[1;32m"       
 Red="\u001b[31m"         
 Yellow="\033[0;33m"
@@ -14,12 +15,12 @@ def checkpoint(rawfile):
     is_exist=os.path.exists(rawfile)
     if is_exist==True:
 
-        print(f"{Green}[RESULT]{reset} Input file [{Green} {rawfile} {reset}] exists.")
+        print(f"[ {Green}RESULT{reset} ] Input file [{Green} {rawfile} {reset}] exists.")
 
         file_type = magic.from_file(rawfile,mime=True)
 
         if file_type == "image/png":
-            print(f"{Green}[RESULT]{reset} This is PNG image file.")
+            print(f"[ {Green}RESULT{reset} ] This is PNG image file.")
 
             handle_img=Image.open(rawfile).convert("RGB")
             filename=rawfile.split(".")[0]
@@ -28,7 +29,8 @@ def checkpoint(rawfile):
             final_img_name=f"{filename}.png"
 
         elif file_type == "image/jpg" or file_type == "image/jpeg":
-            convert=input(f"{Yellow}[!]{reset} This is JPG/JPEG image file.Do u want to convert this JPG/JPEG to PNG file? [{Yellow} y/n {reset}] : ").lower()
+            convert=input(f"\n       {Yellow}[!]{reset} This is JPG/JPEG image file.Do u want to convert this JPG/JPEG to PNG file? [{Yellow} y/n {reset}] : ").lower()
+            print("")
             
             if convert=="y":
                 handle_img=Image.open(rawfile).convert("RGB")
@@ -37,16 +39,16 @@ def checkpoint(rawfile):
                 handle_img.close()
                 final_img_name=f"{filename}.png"
 
-                print(f"{Green}[RESULT]{reset} Created {final_img_name}")
+                print(f"[ {Green}RESULT{reset} ] Created {final_img_name}")
 
             elif convert=="n":
                 print("Goodbye.")
                 exit()
         else:
-            print(f"[{Red}RESULT{reset}] Sorry.We only accept PNG and JPG/JPEG file.")
+            print(f"[ {Red}RESULT{reset} ] Sorry.We only accept PNG and JPG/JPEG file.")
             exit()
     else:
-        print(f"{Red}[RESULT{reset}] File doesn't exist.")
+        print(f"[ {Red}RESULT{reset} ] File doesn't exist.")
         exit()
     return final_img_name
         
@@ -59,38 +61,45 @@ def to_bin(data):
     elif isinstance(data, int) or isinstance(data, np.uint8):
         return format(data, "08b")
     else:
-        raise TypeError(f"{Red}[ERROR]{reset} Type not supported.")
+        raise TypeError(f"[ {Red}ERROR{reset} ] Type not supported.")
 
 def data_encryption():
-
-    key=Fernet.generate_key()
-    clean_key = key.decode('utf-8')
-
-    print("-"*60,
+     
+    generate = input(f"[ {Yellow}TODO{reset} ]   Generate new key (must generate new key if u are first time user) : [ {Yellow}y/n{reset} ]").lower()
+    if generate == "y":
+        key = Fernet.generate_key()
+        clean_key = key.decode('utf-8')
+        print("-"*60,
           f"\n{Green}Access key : {reset}{clean_key}")
-    f=open("access.key","w")
-    f.write(clean_key)
-    f.write("\n[!] Keep this key in a safe place.\n")
-    f.close()
-    print(f"[{Green}RESULT{reset}] Access Key is saved as [{Green} access.key {reset}] !")
-    print("-"*60)
+        f=open("access.key","w")
+        f.write(clean_key)
+        f.write("\n[!] Keep this key in a safe place.\n")
+        f.close()
+        print(f"[ {Green}RESULT{reset} ] Access Key is saved as [{Green} access.key {reset}] !")
+        print("-"*60)
+
+    elif generate == "n":
+        key = input(f"[ {Yellow}TODO{reset} ]   Enter key : ").encode()
+        pass
+    else:
+            print(f"[ {Red}ERROR{reset} ] You must choose one option to encrypt the data.")
 
     fernet=Fernet(key)
 
-    secret_data = input(f"[{Yellow}TODO{reset}]   Enter your message(data) : ").encode("utf-8")
+    secret_data = input(f"[ {Yellow}TODO{reset} ]   Enter your message(data) : ").encode("utf-8")
     clean_encoded_msg = fernet.encrypt(secret_data).decode('utf-8')
-    print(f"[{Green}RESULT{reset}] Encrypted data           : {clean_encoded_msg}")
+    print(f"[ {Green}RESULT{reset} ] Encrypted data           : {clean_encoded_msg}")
 
     # add stop sign
-    clean_encoded_msg += "stop"
+    clean_encoded_msg += "##THW##"
     binary_secret_data = to_bin(clean_encoded_msg)
 
     return binary_secret_data
 
 def image_encode():
-    print(f"\n{Green}[ Initial Checkpoint ]{reset}")
+    print(f"\n{Green}-- Initial Checkpoint --{reset}\n")
 
-    rawfile = input(f"{Yellow}[TODO]{reset}   Enter Image : ")
+    rawfile = input(f"[ {Yellow}TODO{reset} ]   Enter Image : ")
     final_img_name=checkpoint(rawfile)
 
     path, file = os.path.split(final_img_name)
@@ -102,8 +111,9 @@ def image_encode():
 
     binary_secret_data=data_encryption()
     binary_secret_data_size = len(binary_secret_data)
+    
     if binary_secret_data_size > image_size:
-        raise ValueError("{Red}[ERROR]{reset} Insufficient bytes, need bigger image or less data.")
+        raise ValueError("[ {Red}ERROR{reset} ] Insufficient bytes, need bigger image or less data.")
 
     data_index = 0
     for row in image:
@@ -123,31 +133,31 @@ def image_encode():
                 break
 
     cv2.imwrite(output_image,image)
-    print(f"[{Green}RESULT{reset}] Saved encoded image      : {output_image}\n")
+    print(f"[ {Green}RESULT{reset} ] Saved encoded image      : {output_image}\n")
 
 def image_decode():
     print("-"*60)
-    to_decode = input(f"[{Yellow}TODO{reset}]   Enter your encoded image : ")
+    to_decode = input(f"[ {Yellow}TODO{reset} ]   Enter your encoded image : ")
     is_exist=os.path.exists(to_decode)
     if is_exist==True:
         pass
     else:
-        print(f"{Red}[RESULT]{reset} File doesn't exist.")
+        print(f"[ {Red}RESULT{reset} ] File doesn't exist.")
         exit()
 
     decoded_data = data_decryption(to_decode)
     print("-"*60)
-    print(f"[{Green}RESULT{reset}] Decoded data     : {decoded_data}")
-    key=input(f"[{Yellow}TODO{reset}]   Enter access key : ").encode()
+    print(f"[ {Green}RESULT{reset} ] Decoded data     : {decoded_data}")
+    key=input(f"[ {Yellow}TODO{reset} ]   Enter access key : ").encode()
     f=Fernet(key)
 
     decoded_data=decoded_data.encode()
     decrypt = f.decrypt(decoded_data)
     clean_decrypt = decrypt.decode('utf-8')
-    print(f"[{Green}RESULT{reset}] Decrypted data   : {clean_decrypt}\n")
+    print(f"[ {Green}RESULT{reset} ] Decrypted data   : {clean_decrypt}\n")
 
 def data_decryption(image_name):
-    print(f"[{Green}RESULT{reset}] Decoding image to retrieve the encrypted data.Please wait.")
+    print(f"[ {Green}RESULT{reset} ] Decoding image to retrieve the encrypted data.Please wait.")
     image = cv2.imread(image_name)
     binary_data = ""
     for row in image:
@@ -162,18 +172,19 @@ def data_decryption(image_name):
     decoded_data = ""
     for byte in all_bytes:
         decoded_data += chr(int(byte, 2))
-        if decoded_data[-4:] == "stop":
+        if decoded_data[-7:] == "##THW##":
             break
-    return decoded_data[:-4]
+    return decoded_data[:-7]
 
 def main(): 
-
-    print(f"""
-    {Yellow}H I D E & S E E K 
-    -----------------
-    """)
-
-    print(f"{Green}[ HIDE ]{reset}  Enter [ 1 ] to Hide the data in the image\n{Green}[ SEEK ]{reset}  Enter [ 2 ] to Reveal the data from the encoded image\n")
+    
+    banner = f"""{Cyan}
+    H I D E & S E E K  
+   {Yellow}-------------------[ developed by ZuS ]{reset}
+   {reset}"""
+    
+    print(banner)
+    print(f"[ {Green}HIDE{reset} ] : Enter [ 1 ] to Hide the data in the image.\n[ {Green}SEEK{reset} ] : Enter [ 2 ] to Reveal the data from the encoded image.\n")
 
     a = int(input("[ Hide/Seek ] : "))
     if(a==1): 
@@ -181,7 +192,9 @@ def main():
     elif(a==2):
         image_decode()
     else:
-        print(f"{Red}[ERROR]{reset} Please enter 1 or 2.")
+        print(f"[ {Red}ERROR{reset} ] Please enter 1 or 2.")
         exit()
+
 #Here we goooo!!!
-main()
+if __name__ == '__main__':
+    main()
